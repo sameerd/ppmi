50766974e8ea4493da6e21eaa5b909d39c01fd20
Sameer <sld0465@bw.cluster.phystherapy.northwestern.edu>
2018-10-07 15:21:58 -0500

    Directly using XNAT's import service

--- scripts/shell/upload_to_nunda.sh
+++ scripts/shell/upload_to_nunda.sh
@@ -17,53 +17,50 @@ set -x
 # FIXME: Test if this works with post instead of put
 #curl -u ${XNAT_USER}:${XNAT_PASS} -X POST "${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105" -F "file=@3105.xml"
 
+
+SUBJECT_DATA_DIR="/home/sld0465/projects/ppmi/data_collection/working/raw_zip/3105"
+XNAT_EXPT="${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105/experiments/Baseline"
+
+TMPDIR=/home/sld0465/tmp/one_subject
+mkdir -p TMPDIR
+cd $TMPDIR
+
+cp -r ~/projects/ppmi/data_collection/working/raw_zip/3105/Baseline BL
+cp -r ~/projects/ppmi/data_collection/working/raw_zip/3105/Month\ 12 V04
+cp -r ~/projects/ppmi/data_collection/working/raw_zip/3105/Month\ 24 V06
+cp -r ~/projects/ppmi/data_collection/working/raw_zip/3105/Month\ 36 V08
+
+zip -r 3105_BL.zip BL
+zip -r 3105_V04.zip V04
+zip -r 3105_V06.zip V06
+zip -r 3105_V08.zip V08
+
 # Add Baseline Experiment
-#curl -u ${XNAT_USER}:${XNAT_PASS} -X PUT "${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105/experiments/BL?xnat:mrSessionData/date=03/24/2011" 
+#curl -u ${XNAT_USER}:${XNAT_PASS} -X PUT "${XNAT_EXPT}?xnat:mrSessionData/date=03/24/2011" 
 
-SUBJECT_DATA_DIR="/home/sld0465/projects/ppmi/data_collection/output/3105"
+curl -u ${XNAT_USER}:${XNAT_PASS} -X POST --form project=PPMI_DTI \
+	--form subject=3105 \
+	--form session=3105_BL \
+	--form image_archive=@/home/sld0465/tmp/one_subject/3105_BL.zip \
+	"${XNAT_ROOT}/data/services/import"
 
-# FIXME: Add original PPMI xml to some resources directory
+curl -u ${XNAT_USER}:${XNAT_PASS} -X POST --form project=PPMI_DTI \
+	--form subject=3105 \
+	--form session=3105_V04 \
+	--form image_archive=@/home/sld0465/tmp/one_subject/3105_V04.zip \
+	"${XNAT_ROOT}/data/services/import"
 
-## Add DTI_1 scan
-#curl -u ${XNAT_USER}:${XNAT_PASS} -X PUT "${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105/experiments/BL/scans/BL_DTI_1?xsiType=xnat:mrScanData&mrScanData/type=DTI"
-## Add DTI_1 scan resources directory
-#curl -u ${XNAT_USER}:${XNAT_PASS} -X PUT "${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105/experiments/BL/scans/BL_DTI_1/resources/DICOM?format=DICOM&content=DTI_RAW"
-## Add DTI_1 scan resources files
-#curl -u ${XNAT_USER}:${XNAT_PASS} -X POST "${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105/experiments/BL/scans/BL_DTI_1/resources/DICOM/files?extract=true" -F "file=@${SUBJECT_DATA_DIR}/Baseline/DTI_1/dcm.zip"
+curl -u ${XNAT_USER}:${XNAT_PASS} -X POST --form project=PPMI_DTI \
+	--form subject=3105 \
+	--form session=3105_V06 \
+	--form image_archive=@/home/sld0465/tmp/one_subject/3105_V06.zip \
+	"${XNAT_ROOT}/data/services/import"
 
-## Add DTI_2 scan
-#curl -u ${XNAT_USER}:${XNAT_PASS} -X PUT "${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105/experiments/BL/scans/BL_DTI_2?xsiType=xnat:mrScanData&mrScanData/type=DTI"
-## Add DTI_2 scan resources directory
-#curl -u ${XNAT_USER}:${XNAT_PASS} -X PUT "${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105/experiments/BL/scans/BL_DTI_2/resources/DICOM?format=DICOM&content=DTI_RAW"
-## Add DTI_2 scan resources files
-#curl -u ${XNAT_USER}:${XNAT_PASS} -X POST "${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105/experiments/BL/scans/BL_DTI_2/resources/DICOM/files?extract=true" -F "file=@${SUBJECT_DATA_DIR}/Baseline/DTI_2/dcm.zip"
-
-## Add T1_1 scan
-#curl -u ${XNAT_USER}:${XNAT_PASS} -X PUT "${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105/experiments/BL/scans/BL_T1_1?xsiType=xnat:mrScanData&mrScanData/type=MPRAGE"
-## Add T1_1 scan resources directory
-#curl -u ${XNAT_USER}:${XNAT_PASS} -X PUT "${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105/experiments/BL/scans/BL_T1_1/resources/DICOM?format=DICOM&content=MPRAGE_RAW"
-## Add T1_1 scan resources files
-#curl -u ${XNAT_USER}:${XNAT_PASS} -X POST "${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105/experiments/BL/scans/BL_T1_1/resources/DICOM/files?extract=true" -F "file=@${SUBJECT_DATA_DIR}/Baseline/T1_1/dcm.zip"
-
-## Add T2_1 scan
-#curl -u ${XNAT_USER}:${XNAT_PASS} -X PUT "${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105/experiments/BL/scans/BL_T2_1?xsiType=xnat:mrScanData&mrScanData/type=T2"
-## Add T2_1 scan resources directory
-#curl -u ${XNAT_USER}:${XNAT_PASS} -X PUT "${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105/experiments/BL/scans/BL_T2_1/resources/DICOM?format=DICOM&content=T2_RAW"
-## Add T2_1 scan resources files
-#curl -u ${XNAT_USER}:${XNAT_PASS} -X POST "${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105/experiments/BL/scans/BL_T2_1/resources/DICOM/files?extract=true" -F "file=@${SUBJECT_DATA_DIR}/Baseline/T2_1/dcm.zip"
-
-## Add PD_1 scan
-#curl -u ${XNAT_USER}:${XNAT_PASS} -X PUT "${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105/experiments/BL/scans/BL_PD_1?xsiType=xnat:mrScanData&mrScanData/type=T2_PD"
-## Add T2_1 scan resources directory
-#curl -u ${XNAT_USER}:${XNAT_PASS} -X PUT "${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105/experiments/BL/scans/BL_PD_1/resources/DICOM?format=DICOM&content=T2_PD_RAW"
-## Add T2_1 scan resources files
-#curl -u ${XNAT_USER}:${XNAT_PASS} -X POST "${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105/experiments/BL/scans/BL_PD_1/resources/DICOM/files?extract=true" -F "file=@${SUBJECT_DATA_DIR}/Baseline/PD_1/dcm.zip"
-
-
-# Trigger pipelines
-# FIXME: Use import service
-# curl -u ${XNAT_USER}:${XNAT_PASS} -X PUT "${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105/experiments/BL?triggerPipelines=true&pullDataFromHeaders=true&fixScanTypes=true"
-
+curl -u ${XNAT_USER}:${XNAT_PASS} -X POST --form project=PPMI_DTI \
+	--form subject=3105 \
+	--form session=3105_V08 \
+	--form image_archive=@/home/sld0465/tmp/one_subject/3105_V08.zip \
+	"${XNAT_ROOT}/data/services/import"
 
 # Delete subject
 # curl -u ${XNAT_USER}:${XNAT_PASS} -X DELETE "${XNAT_ROOT}/data/projects/PPMI_DTI/subjects/3105"
